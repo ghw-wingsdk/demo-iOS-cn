d7e829ef6253006a1a990236b1031367f9e1cf23
hank <hank.zhang@proficientcity.com>
2017-04-13 11:22:53 +0800

    添加session 发送

--- WADemocn-ios/WADemocn/WADemoAppDelegate.m
+++ WADemocn-ios/WADemocn/WADemoAppDelegate.m
@@ -39,10 +39,12 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save us er data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [WAUserProxy applicationDidEnterBackground:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [WAUserProxy applicationWillEnterForeground:application];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
